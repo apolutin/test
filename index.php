8b2b14fa7b4357e6b07224bfacab60774d477423
apolutin <polutin@bk.ru>
2015-04-23 19:46:58 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?
+header("Content-Type: text/html; charset=utf-8");
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Результаты игры</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <iframe src="form.htm">Фреймы не поддерживаются</iframe>
+  <iframe src="results.htm">Фреймы не поддерживаются</iframe>
+</body>
+</html>
